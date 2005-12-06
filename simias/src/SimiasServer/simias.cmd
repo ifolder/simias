e8667f8da7c4e7b8c70fcc0e9e11f9993ab2dbc2
bradya <bradya@1a4d904c-e006-0410-be0b-b506c45ff692>
2005-12-06 22:27:02 +0000

    changes for CASA integration
    git-svn-id: svn://svn.code.sf.net/p/simias/code/branches/casa@5693 1a4d904c-e006-0410-be0b-b506c45ff692

---
+++ simias/src/SimiasServer/simias.cmd
@@ -0,0 +1,14 @@
+@echo off
+rem ######################################################################
+rem
+rem  $RCSfile$
+rem
+rem  Copyright (C) 2004 Novell, Inc.
+rem
+rem  Author: Rob
+rem
+rem ######################################################################
+
+cd %0%\..
+
+@SimiasServer.exe %*
