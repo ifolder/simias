1848591444449515af7d30c38f8046867aec3cbf
bradya <bradya@1a4d904c-e006-0410-be0b-b506c45ff692>
2006-02-01 14:22:30 +0000

    implementing...
    git-svn-id: svn://svn.code.sf.net/p/simias/code/trunk@5885 1a4d904c-e006-0410-be0b-b506c45ff692

--- Simias.Rss/FeedSync/FeedSync.cs
+++ Simias.Rss/FeedSync/FeedSync.cs
@@ -40,7 +40,8 @@ namespace Simias.RssClient
 		private string localPath;
 		private string user;
 		private string password;
-		static private string FeedDirectory = "SimiasFeeds";
+		private string itemDirectory;
+		static private string FeedsDirectory = "SimiasFeeds";
 		static private string DefaultFeedDirectory = "My Feeds";
 		#endregion
 
@@ -175,22 +176,28 @@ namespace Simias.RssClient
 			syncElem.InnerText = "0";
 			feedElem.AppendChild( syncElem );
 
+			XmlNode itemElem = document.CreateNode( XmlNodeType.Element, "ItemDirectory", "");
+
+			this.itemDirectory = Guid.NewGuid().ToString();
+			itemElem.InnerText = itemDirectory;
+			feedElem.AppendChild( itemElem );
+
+			string fullItemDirectoryPath = 
+				GetFeedsPath() + Path.DirectorySeparatorChar + this.itemDirectory;
+
 			//Console.WriteLine("Add the new element to the document...");
 			document.AppendChild( feedElem );
 
+			bool itemDirCreated = false;
+
 			try
 			{
+				// Create the item directory
+				Directory.CreateDirectory( fullItemDirectoryPath );
+				itemDirCreated = true;
 				XmlTextWriter tw = new XmlTextWriter( feedPath, null);
 				tw.Formatting = Formatting.Indented;
 				document.Save( tw );
-
-				/*
-				FileStream fileStream = System.IO.File.Create( feedPath );
-				XmlTextWriter writer = new XmlTextWriter( fileStream as TextWriter );
-				writer.Formatting = Formatting.Indented;
-				document.WriteTo( writer );
-				writer.Flush();
-				*/
 
 				return true;
 			}
@@ -198,7 +205,14 @@ namespace Simias.RssClient
 			{
 				Console.WriteLine( "ERROR: failed to create feed configuration file: " + feedPath );
 				Console.WriteLine( ex.Message );
-				// log
+
+				// If the item directory was created delete it
+				if ( itemDirCreated == true )
+				{
+					Directory.Delete( fullItemDirectoryPath );
+				}
+
+				itemDirectory = null;
 			}
 
 			// Create the file and stream the document out
@@ -214,6 +228,26 @@ namespace Simias.RssClient
 		/// </summary>
 		private string GetFeedConfigurationPath( )
 		{
+			string feedPath = null;
+			string feedsPath = GetFeedsPath();
+			if ( feedsPath != null )
+			{
+				// assemble the full feed path
+				feedPath = feedsPath + Path.DirectorySeparatorChar.ToString() + this.feed + ".xml";
+			}
+
+			return feedPath;
+		}
+
+		/// <summary>
+		/// Private method to return the full path to the
+		/// configuration file for a subscribed feed.
+		/// 
+		/// Note: expects local member "feed" to contain the name
+		/// of the feed the caller is removing.
+		/// </summary>
+		private string GetFeedsPath( )
+		{
 			string path = Environment.GetFolderPath( Environment.SpecialFolder.LocalApplicationData );
 			if ( ( path == null ) || ( path.Length == 0 ) )
 			{
@@ -221,17 +255,15 @@ namespace Simias.RssClient
 			}
 
 			// and our feeds directory
-			path = path + Path.DirectorySeparatorChar.ToString() + FeedSync.FeedDirectory;
+			path = path + Path.DirectorySeparatorChar.ToString() + FeedSync.FeedsDirectory;
 			if ( !Directory.Exists( path ) )
 			{
 				return null;
 				//Directory.CreateDirectory( path );
 			}
 
-			// assemble the full feed path
-			return path + Path.DirectorySeparatorChar.ToString() + this.feed + ".xml";
+			return path;
 		}
-
 		/// <summary>
 		/// Private method to a subscription from the system.
 		/// If param content == true remove the content (enclosures) 
@@ -322,159 +354,29 @@ namespace Simias.RssClient
 		#endregion
 
 		#region Public Methods
-		public void ProcessFeed( )
+		/// Method to sync the actual enclosures down to the
+		/// local file system
+		/// </summary>
+		public void ProcessFeed( XmlDocument doc )
 		{
-			Uri serviceUri = new Uri( url );
-			HttpWebResponse response = null;
-			CookieContainer cookieJar = new CookieContainer();
-
-			// Build a credential from the user name and password.
-			NetworkCredential credentials = null;
-			if ( user != null && password != null )
-			{
-				credentials = new NetworkCredential( user, password ); 
-			}
-
-			// Create the web request.
-			HttpWebRequest request = (HttpWebRequest)WebRequest.Create( serviceUri );
-			bool retry = true;
-		
-		proxyRetry:
-
-			request.Credentials = credentials;
-			request.Timeout = 15 * 1000;
-			request.CookieContainer = cookieJar;
-			//request.Proxy = ProxyState.GetProxyState( request.RequestUri );
-
 			try
 			{
-				// Get the response from the web server.
-				response = request.GetResponse() as HttpWebResponse;
-
-				// Mono has a bug where it doesn't set the cookies in the cookie jar.
-				cookieJar.Add( response.Cookies );
-			}
-			catch ( WebException we )
-			{
-				IsTrustFailure( serviceUri.Host, we );
-				if ( ( we.Status == WebExceptionStatus.Timeout ) ||
-					( we.Status == WebExceptionStatus.NameResolutionFailure ) )
+				foreach( XmlNode itemNode in doc.DocumentElement.SelectNodes( "//item" ) )
 				{
-					throw we;	
-				}
-				else
-				{
-					response = we.Response as HttpWebResponse;
-					if (response != null)
+					Console.WriteLine( "Processing item: " + itemNode.InnerText );
+					foreach( XmlNode child in itemNode.ChildNodes )
 					{
-						cookieJar.Add( response.Cookies );
-						if ( response.StatusCode == HttpStatusCode.Unauthorized && retry == true )
-						{
-							// This should be a free call we must be behind iChain.
-							request = (HttpWebRequest)WebRequest.Create( response.ResponseUri );
-							retry = false;
-							goto proxyRetry;
-						}
+						Console.WriteLine( 
+							"  tag: {0} value: {0}",
+							child.Name,
+							child.InnerText );
 					}
-					response = null;
-				}
-			}
-			
-			// Make sure that there was an answer.
-			if ( response != null )
-			{
-				try
-				{
-					// Get the stream associated with the response.
-					Stream receiveStream = response.GetResponseStream();
 					
-					// Pipes the stream to a higher level stream reader with the required encoding format. 
-					StreamReader readStream = new StreamReader( receiveStream, Encoding.UTF8 );
-
-					//Console.WriteLine( readStream.ReadToEnd() );
-
-					try
-					{
-						XmlDocument document = new XmlDocument();
-						document.Load( readStream );
-
-						//Create an XmlNamespaceManager for resolving namespaces.
-						//XmlNamespaceManager nsmgr = new XmlNamespaceManager( document.NameTable );
-						//nsmgr.AddNamespace( "wsil", document.DocumentElement.NamespaceURI );
-
-						// Search for the named service element.
-//						string queryString = "rss/channel/[contains(title," + channel + ")]";
-//						string queryString = "rss/channel/[title = \'" + channel + "\']";
-						string queryString = "//title";
-						XmlNode serviceNode = document.DocumentElement.SelectSingleNode( queryString );
-						if ( serviceNode != null )
-						{
-							Console.WriteLine( "found channel" );
-							Console.WriteLine( serviceNode.ToString() );
-
-							/*
-							// Get the description node.
-							XmlElement description = serviceNode[ WSIL_DescriptionTag ];
-							if ( description != null )
-							{
-								// Get the uri location.
-								string uriString = description.GetAttribute( WSIL_LocationAttrTag );
-								if ( uriString != null )
-								{
-									// Fix up the URI if it is relative.
-									if ( !uriString.ToLower().StartsWith( Uri.UriSchemeHttp ) )
-									{
-										Uri respUri = response.ResponseUri;
-										UriBuilder urb = new UriBuilder( respUri.Scheme, respUri.Host, respUri.Port, uriString.TrimStart( new char[] { '/' } ) );
-										serviceUrl = urb.Uri;
-										// Check to see if we need to use ssl.
-										// Make the request and see if we get redirected 302;
-										// Create the web request.
-										request = (HttpWebRequest)WebRequest.Create( serviceUrl );
-										request.CookieContainer = cks;
-										request.Proxy = ProxyState.GetProxyState( request.RequestUri );
-										response.Close();
-										try
-										{
-											response = request.GetResponse() as HttpWebResponse;
-											serviceUrl = response.ResponseUri;
-										}
-										catch (WebException wex)
-										{
-											IsTrustFailure(host, wex);
-											response = wex.Response as HttpWebResponse;
-											if (response != null)
-											{
-												if (response.StatusCode == HttpStatusCode.Unauthorized)
-												{
-													if (response.Headers.Get("Simias-Error") != null)
-													{
-														// This is expected because this service requires authentication.
-														serviceUrl = response.ResponseUri;
-													}
-												}
-											}
-										}
-									}
-									else
-									{
-										serviceUrl = new Uri( uriString );
-									}
-								}
-							}
-							*/
-						}
-					}
-					finally
-					{
-						readStream.Close();
-					}
 				}
-				finally
-				{
-					response.Close();
-				}
-			}	
+			}
+			catch( Exception ex )
+			{
+			}
 		}
 
 		/// <summary>
@@ -571,13 +473,6 @@ namespace Simias.RssClient
 						XmlDocument document = new XmlDocument();
 						document.Load( readStream );
 
-						//Create an XmlNamespaceManager for resolving namespaces.
-						//XmlNamespaceManager nsmgr = new XmlNamespaceManager( document.NameTable );
-						//nsmgr.AddNamespace( "wsil", document.DocumentElement.NamespaceURI );
-
-						// Search for the named service element.
-						//						string queryString = "rss/channel/[contains(title," + channel + ")]";
-						//						string queryString = "rss/channel/[title = \'" + channel + "\']";
 						XmlNode serviceNode = document.DocumentElement.SelectSingleNode( "//title" );
 						if ( serviceNode != null )
 						{
@@ -601,13 +496,13 @@ namespace Simias.RssClient
 							tempNode = document.DocumentElement.SelectSingleNode( "//link" );
 							if ( tempNode != null )
 							{
-								description = tempNode.InnerText;
+								link = tempNode.InnerText;
 							}
 
 							// Create the configuration file
 							if ( this.CreateFeedConfiguration( link, description, ttl ) == true )
 							{
-								this.ProcessFeed();
+								this.ProcessFeed( document );
 							}
 						}
 					}
