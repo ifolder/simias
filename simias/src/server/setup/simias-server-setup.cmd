0c6c06d3dde3f90aa06470d1651e346cd7ddffca
amahabaleshwar <amahabaleshwar@1a4d904c-e006-0410-be0b-b506c45ff692>
2007-08-10 04:26:59 +0000

    CopyRight Changes
    git-svn-id: svn://svn.code.sf.net/p/simias/code/trunk@6968 1a4d904c-e006-0410-be0b-b506c45ff692

--- simias/src/server/setup/simias-server-setup.cmd
+++ simias/src/server/setup/simias-server-setup.cmd
@@ -1,13 +1,26 @@
 @echo off
-rem ######################################################################
-rem
-rem  $RCSfile: simias-server-setup.cmd,v $
-rem
-rem  Copyright (C) 2004 Novell, Inc.
-rem
-rem  Author: Rob
-rem
-rem ######################################################################
+rem  /****************************************************************************
+rem  |
+rem  | Copyright (c) 2007 Novell, Inc.
+rem  | All Rights Reserved.
+rem  |
+rem  | This program is free software; you can redistribute it and/or
+rem  | modify it under the terms of version 2 of the GNU General Public License as
+rem  | published by the Free Software Foundation.
+rem  |
+rem  | This program is distributed in the hope that it will be useful,
+rem  | but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem  | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem  | GNU General Public License for more details.
+rem  |
+rem  | You should have received a copy of the GNU General Public License
+rem  | along with this program; if not, contact Novell, Inc.
+rem  |
+rem  | To contact Novell about this file by physical or electronic mail,
+rem  | you may find current contact information at www.novell.com
+rem  |
+rem  |  Author: Rob
+rem  |***************************************************************************/
 
 cd %0%\..
 
