25d7870ce986f1ca27be01600fe018487059e6be
hegdegg <hegdegg@1a4d904c-e006-0410-be0b-b506c45ff692>
2010-02-17 06:48:46 +0000

    Corrected spelling mistake
    git-svn-id: svn://svn.code.sf.net/p/simias/code/trunk@7311 1a4d904c-e006-0410-be0b-b506c45ff692

--- src/core/SimiasApp/Properties/AssemblyInfo.cs
+++ src/core/SimiasApp/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Novel iFolder Core")]
+[assembly: AssemblyTitle("Novell iFolder Core")]
 [assembly: AssemblyDescription("Simias Executable for Web Service Communication")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Novell Inc")]
